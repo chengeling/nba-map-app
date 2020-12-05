91557e744c8f5ebdff473ae77ab9ac2328b39c66
chengeling <przemek103@op.pl>
2020-12-05 20:36:49 +0100

    Removed map, add background video

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
 const hideMenuButton = document.querySelector('.app-header__btn')
-const showMenuButton = document.querySelector('.app-right-panel__btn')
-const menu = document.querySelector('.app-header');
+const openMenuButton = document.querySelector('.app-right-panel__btn')
+const menu = document.querySelector('.app-header--hidden');
+
+// hideMenuButton.addEventListener('click', () => {
+//     menu.classList.add('app-header--open')
+// });
+
+// showMenuButton.addEventListener('click', () => {
+//     menu.classList.remove('app-header--open')
+// });
+
+openMenuButton.addEventListener('click', () => {
+  menu.classList.add('app-header--open');
+})
 
 hideMenuButton.addEventListener('click', () => {
-    menu.classList.add('app-header--hide')
-});
-
-showMenuButton.addEventListener('click', () => {
-    menu.classList.remove('app-header--hide')
-});
-
-require(["esri/views/MapView", "esri/WebMap"], function (
-    MapView,
-    WebMap
-  ) {
-    const webmap = new WebMap({
-      portalItem: {
-        // autocasts as new PortalItem()
-        id: "61c7ed9259fb4c1083f2964c402c37d0"
-      }
-    });
-
-    /************************************************************
-     * Set the WebMap instance to the map property in a MapView.
-     ************************************************************/
-    const view = new MapView({
-      map: webmap,
-      container: "app-right-panel__map"
-    });
-  });
+  menu.classList.remove('app-header--open');
+})
