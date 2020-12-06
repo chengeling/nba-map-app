a8a0e1974c3081806b0ab14d9e15cd01d1750f8f
chengeling <przemek103@op.pl>
2020-12-06 14:41:27 +0100

    created about page

--- js/script.js
+++ js/script.js
@@ -2,18 +2,10 @@ const hideMenuButton = document.querySelector('.app-header__btn')
 const openMenuButton = document.querySelector('.app-right-panel__btn')
 const menu = document.querySelector('.app-header--hidden');
 
-// hideMenuButton.addEventListener('click', () => {
-//     menu.classList.add('app-header--open')
-// });
-
-// showMenuButton.addEventListener('click', () => {
-//     menu.classList.remove('app-header--open')
-// });
-
 openMenuButton.addEventListener('click', () => {
   menu.classList.add('app-header--open');
 })
 
-hideMenuButton.addEventListener('click', () => {
+menu.addEventListener('click', () => {
   menu.classList.remove('app-header--open');
-})
+}) 
